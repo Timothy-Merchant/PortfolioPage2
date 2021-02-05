01cb10fcde2e13716cdbc2cea83e410dbf2609f7
Tim <ryalin1286@gmail.com>
2021-02-05 21:43:52 +0000

    Initial setup completed

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/styles.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('css/'))        
+});
+
+gulp.task('watch', gulp.series('sass'), function () {
+    gulp.watch('portfoliopage/scss/**/*.scss', ['sass']);
+    // Other watchers
+});
