d8d1a395c142d6d21c89a2e1a97e6c008c9f6e1a
Tim <ryalin1286@gmail.com>
2021-02-16 19:11:53 +0000

    Added HTML layout and basic styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+let watch = require('gulp-watch')
+
+gulp.task('minify-css', () => {
+    return gulp.src('css/style.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./css/'));
+})
 
 gulp.task('sass', function () {
-    return gulp.src('scss/*.scss')
-        .pipe(sass()) // Using gulp-sass
-        .pipe(gulp.dest('css/'))        
+	return gulp.src('./scss/styles.scss')
+		.pipe(sass())
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('./css/'));
 });
 
-gulp.task('watch', gulp.series('sass'), function () {
-    gulp.watch('portfoliopage/scss/**/*.scss', ['sass']);
-    // Other watchers
-});
+gulp.task('minify-sass', gulp.series('sass', 'minify-css'));
+
+// gulp.watch
+gulp.task('watch', function () {
+    // Watch for any file that ends in .scss in any directory:
+    return gulp.watch('scss/**/*.scss',
+        // If you find one, I want you to run minify sass:
+    gulp.task('minify-sass'));
+})
